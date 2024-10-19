8d6c473ff7d67940b6662e7d11640a950fd73cd2
Kodson <kodsontransport@gmail.com>
2024-10-19 16:23:59 -0700

    https error correction

--- src/main/java/com/kodsonApp/KodsonApplication.java
+++ src/main/java/com/kodsonApp/KodsonApplication.java
@@ -22,7 +22,7 @@ public class KodsonApplication {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
 		corsConfiguration.setAllowedOrigins(Arrays.asList("http://192.168.88.237:2443", "http://41.139.44.167:2443",
-				"https://192.168.88.237:2443", "https://41.139.44.167:2443",
+				"https://192.168.88.237:2443", "https://41.139.44.167:2443","https://kodsonsystems.com:2443",
 				"http://localhost:2443", "http://frontend:2443", "http://localhost:3000","http://localhost:3001"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
