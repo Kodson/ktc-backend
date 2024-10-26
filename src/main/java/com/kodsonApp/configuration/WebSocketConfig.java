38a31c7efa205bd0de20ac268e1ccdb69911812d
Kodson <kodsontransport@gmail.com>
2024-10-26 16:09:51 -0700

    fixing socket correction

--- src/main/java/com/kodsonApp/configuration/WebSocketConfig.java
+++ src/main/java/com/kodsonApp/configuration/WebSocketConfig.java
@@ -11,9 +11,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/api").setAllowedOrigins("*").withSockJS();
     }
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
