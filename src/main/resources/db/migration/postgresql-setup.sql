e132feae798ac7ca24b9620154eb2435c88951fb
kodson <iconmaxwellsowusu@gmail.com>
2026-09-03 03:37:08 -0600

    testing the workflow and docker push

--- src/main/resources/db/migration/postgresql-setup.sql
+++ src/main/resources/db/migration/postgresql-setup.sql
@@ -2,21 +2,21 @@
 -- Run this script to create the database and user for KodsonApi
 
 -- Create database
-CREATE DATABASE kodsonplus;
+CREATE DATABASE ktc_postgress;
 
 -- Create user
-CREATE USER kodsonuser WITH ENCRYPTED PASSWORD 'kodsonpass';
+CREATE USER ktc_kodson_user WITH ENCRYPTED PASSWORD '0040105715@Kodson12345';
 
 -- Grant privileges
-GRANT ALL PRIVILEGES ON DATABASE kodsonplus TO kodsonuser;
+GRANT ALL PRIVILEGES ON DATABASE ktc_postgress TO ktc_kodson_user;
 
 -- Connect to the database
-\c kodsonplus;
+\c ktc_postgress;
 
 -- Grant schema privileges
-GRANT ALL ON SCHEMA public TO kodsonuser;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO kodsonuser;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO kodsonuser;
+GRANT ALL ON SCHEMA public TO ktc_kodson_user;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO ktc_kodson_user;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO ktc_kodson_user;
 
 -- Create indexes for better performance (run after tables are created)
 -- These will be created automatically by Hibernate, but we can optimize them
